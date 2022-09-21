90c8d5dfbdddee96834c8c7c4259c7e09eda1ad4
KrushalKalal <krushalkalal@gmail.com>
2022-09-22 02:53:28 +0530

    cuisine cost filter

--- src/component/Listing/ListingComponent.js
+++ src/component/Listing/ListingComponent.js
@@ -1,9 +1,11 @@
 import React,{Component} from 'react';
 import axios from 'axios';
-import Filter from './Filter'
+import CuisineFilter from './Filter/CuisineFilter';
+import CostFilter from './Filter/CostFilter';
 import RestaurantList from './RestaurantList';
 import Header from './Header'
 import './RestaurantList.css'
+import './Filter.css'
 
 const optionList = "https://xomato-api.herokuapp.com/api/restaurantList?optionId="
 
@@ -16,13 +18,29 @@ class ListingComponent extends Component{
         }
     }
 
+    setDataPerFilter = (data) => {
+      this.setState({options:data})
+  }
+
     render(){
         return(
          <>
           <Header/>
           <section class="container-fluid">
              <div class="row">
-                <Filter/>
+                 <div class="col-lg-3 filter">
+                    <div class="filters">
+                      <div class="title">
+                        <span>Filters</span>
+                      </div>
+                      <CuisineFilter optionId={this.props.match.params.optionId}
+                       restPerCuisine={(data) => {this.setDataPerFilter(data)}}
+                      />
+                      <CostFilter optionId={this.props.match.params.optionId}
+                      restPerCost={(data) => {this.setDataPerFilter(data)}}
+                      />
+                     </div>
+                 </div>
                  <div class="col-lg-9 content">
                    <div class="content_block">
                      <h2>Order food online in Ahmedabad</h2>
